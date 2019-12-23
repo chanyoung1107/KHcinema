ba59a9e9c3dbff6a4967fda52e4d0cb9f2916b8e
kor-solidarity <confed00@naver.com>
2019-12-23 20:11:23 +0900

    상영스케쥴을 위한 기본뼈대 작성

---
+++ src/controller/E_ScheduleManager.java
@@ -0,0 +1,26 @@
+package controller;
+
+import model.vo.H_Schedule;
+
+
+public class E_ScheduleManager {
+    // 영화 상영시간을 관리하는 클래스.
+
+    private H_Schedule schedule = new H_Schedule();
+
+    // 현 시간 이후 존재하는 모든 일정 조회
+    public void viewSchedule(){
+
+    }
+
+    // 상영일정 추가
+   public void addSchedule(){
+
+    }
+
+    public void deleteSchedule(){
+
+    }
+
+
+}
